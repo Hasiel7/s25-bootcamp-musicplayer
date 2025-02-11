e0d5773524bc23027176880e3bfb1923f1414735
Jahaziel Herberth <hasiel@ad.unc.edu>
2025-02-10 23:55:17 -0500

    Checkpoint , blending background and cover art

---
+++ Apple Music Recreation/Apple Music Recreation/Apple_Music_RecreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_RecreationApp.swift
+//  Apple Music Recreation
+//
+//  Created by Jahaziel Herberth Martinez on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_RecreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
