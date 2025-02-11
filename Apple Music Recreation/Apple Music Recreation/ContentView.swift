f47a56d6060dd734c4b86562a04b68e8606fd9d2
Jahaziel Herberth <hasiel@ad.unc.edu>
2025-02-10 23:56:27 -0500

    code

--- Apple Music Recreation/Apple Music Recreation/ContentView.swift
+++ Apple Music Recreation/Apple Music Recreation/ContentView.swift
@@ -6,19 +6,73 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var player: AVPlayer? = nil
+    let Smokey = Color(red: 10/255, green: 6/255, blue: 9/255)
+    let TranceColor = Color(red: 0.051, green: 0.024, blue: 0.043)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack{
+                Image(systemName: "chevron.compact.up")
+                    .foregroundStyle(.white)
+                    .padding(.top, 60)
+
+                VideoPlayer(player: player)
+                    .frame(height: 400)
+                    .opacity(0.97)
+                    .cornerRadius(16)
+                    .padding(.top , 50)
+                    .ignoresSafeArea()
+                    
+                HStack{
+                    Text("Trance")
+                        .foregroundColor(.white)
+                        .padding(.bottom, 300)
+                        .fontWeight(.bold)
+                }
+            }
+            .ignoresSafeArea()
+    
         }
-        .padding()
+        .background(
+                    LinearGradient(
+                        gradient: Gradient(colors:[TranceColor, Smokey]),
+                                startPoint:.top,
+                                endPoint:.bottom
+                    )
+            )
+        .ignoresSafeArea()
+        
+        .onAppear {
+            playVideo()
+        }
+    }
+
+    private func playVideo() {
+        if let url = Bundle.main.url(forResource: "trance", withExtension: "mov") {
+            let avPlayer = AVPlayer(url: url)
+            avPlayer.actionAtItemEnd = .none
+
+        
+            NotificationCenter.default.addObserver(
+                forName: .AVPlayerItemDidPlayToEndTime,
+                object: avPlayer.currentItem,
+                queue: .main
+            ) { _ in
+                avPlayer.seek(to: .zero)
+                avPlayer.play()
+            }
+
+            avPlayer.play()
+            player = avPlayer
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
